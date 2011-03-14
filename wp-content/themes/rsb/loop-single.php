71a27ee43db1d41b31b59dc39e027fec10e77ae7
Pjan718 <pjan321@hotmail.com>
2011-03-14 07:07:53 -0400

    single post code

--- wp-content/themes/rsb/loop-single.php
+++ wp-content/themes/rsb/loop-single.php
@@ -1,36 +1,31 @@
-<?php
+<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 
-/*
-Template Name: Snarfer
-*/
-?>
-<?php get_header(); ?>
+		<div class="post" id="post-<?php the_ID(); ?>">
 
+			<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 
-<div id="box">
-    <!--<div class="rotatorArea"></div>-->
-    
-	   <div class="logoArea"></div>
-	    <div class="barInfo">
-    
+			<div class="entry">
+
+				<?php the_content(); ?>
+
+				<p class="postmetadata">
+<?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
+<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
+				</p>
+
+			</div>
+			<div class="comments-template">
+<?php comments_template(); ?>
+</div>
+		</div>
+
+	<?php endwhile; ?>
+
+		<div class="navigation">
+			<?php previous_post_link('%link') ?> <?php next_post_link(' %link') ?>
+		</div>
+
+	<?php endif; ?>
+
 </div>
 
-<div class="mainContent">
-	 
-	<div class="newsHeader"></div>
-
-			<?php
-			/* Run the loop to output the post.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-single.php and that will be used instead.
-			 */
-			get_template_part( 'loop', 'single' );
-			?>
-
-			</div><!-- #content -->
-		</div><!-- #container -->
- <?php get_sidebar(); ?>
-</div>
-
-
-<?php get_footer(); ?>
