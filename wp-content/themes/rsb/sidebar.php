22c3efb38bf6fab7dd3bcab4ad3a20b0310b6834
Pjan718 <pjan321@hotmail.com>
2011-04-02 16:27:07 -0400

    latest

--- wp-content/themes/rsb/sidebar.php
+++ wp-content/themes/rsb/sidebar.php
@@ -14,4 +14,17 @@
 </div>
 
  <div class="eventBottom"></div>  
+
+<div class="socialMedia">
+                <h3>Follow Us On</h3>
+                <ul>
+                    <li><a href="http://www.facebook.com/pages/Roosevelt-Sports-Bar/275226830136">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/social_media_fb.gif" alt="Flushing Sports Bar" ></img></a></li>
+                    <li><img src="<?php bloginfo('template_directory'); ?>/images/social_media_twitter.gif" alt="Roosevelt Twitter" ></img></li>
+                    <li><a href="http://www.flickr.com/photos/38506077@N04" rel="external"><img src="<?php bloginfo('template_directory'); ?>/images/social_media_flickr.gif"
+                    alt="Roosevelt Flickr" ></img></a></li>
+                </ul>
+            </div>
+
+
 </div>
